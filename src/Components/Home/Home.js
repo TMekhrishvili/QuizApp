2070b8feb0a5ff9d150f95d704f6f23e6dade4cf
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2020-09-29 23:09:06 +0400

    Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Home.css'
+
 const Home = () => (
     <div>
-      <Link to="/category">Start Quiz</Link>
+      <Link style={{ textDecoration: 'none' }} to="/category">
+        <div className="container">
+          <div className="start-quiz">
+            <h1 className="start-quiz-text">Start Quiz</h1>
+          </div>
+        </div>
+      </Link>
     </div>
   )
 export default Home
